52d9cee4ecf55544f648601489051261acafbc0f
Student 6 <Student 6@10.8.33.155>
2016-11-15 13:54:02 -0500

    hgfzxtg

--- Cavemanmanmanman/src/katherineRisa/MainEvent.java
+++ Cavemanmanmanman/src/katherineRisa/MainEvent.java
@@ -3,23 +3,47 @@ package katherineRisa;
 public class MainEvent {
 	
 	private static String[][] grid;
-	private static String[][] words = {{}, {}, {}};
+	private static String[][] words = {{"one", "two", "three"}, {"four", "five", "six"}, {"seven", "eight", "nine"}};
 	
 	public MainEvent() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		grid = new String[17][17];
+		grid = new String[16][17];
 		makeGrid(grid);
+		randomizeWords(words);
+		
+		
+//		String test = "abc";
+//		char[] array = test.toCharArray();
+//		
+//		for(int i = 0; i < array.length; i++){
+//			System.out.println(array[i]);	
+//		}
 
+	}
+
+	private static void randomizeWords(String[][] words) {
+		int rndIndx = (int)(Math.random()*words.length);
+		System.out.println(rndIndx);
+		String[] selectedArray = words[rndIndx];
+		
+		char[] charArr = new char[selectedArray.length];
+		
+		for(int i = 0; i < charArr.length; i++){
+			for(int j = 0; j < selectedArray.length; j++){
+				char[] temp = selectedArray[j].toCharArray();
+			}
+		}
+		
 	}
 
 	private static void makeGrid(String[][] array) {
 		for(int row = 0; row < array.length; row++){
 			for(int col = 0; col < array[row].length; col++){
 				array[row][col] = " ";
-				if(col!= 0 && col!= array[row].length - 1 && row % 4 == 0){
+				if(col!= 0 && col!= array[row].length - 1 && row % 3 == 0){
 					array[row][col] = "_";
 				}
 				if(row!= 0 && col % 4 == 0){
