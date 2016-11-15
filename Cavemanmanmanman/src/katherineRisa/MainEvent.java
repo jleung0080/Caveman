33e2a12a93b4fc9c58e7cd4f389db95a8617b04a
moo <moo>
2016-11-15 00:19:00 -0500

    adfafw

--- Cavemanmanmanman/src/katherineRisa/MainEvent.java
+++ Cavemanmanmanman/src/katherineRisa/MainEvent.java
@@ -1,14 +1,43 @@
 package katherineRisa;
 
 public class MainEvent {
-
+	
+	private static String[][] grid;
+	private static String[][] words = {{}, {}, {}};
+	
 	public MainEvent() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		grid = new String[17][17];
+		makeGrid(grid);
 
+	}
+
+	private static void makeGrid(String[][] array) {
+		for(int row = 0; row < array.length; row++){
+			for(int col = 0; col < array[row].length; col++){
+				array[row][col] = " ";
+				if(col!= 0 && col!= array[row].length - 1 && row % 4 == 0){
+					array[row][col] = "_";
+				}
+				if(row!= 0 && col % 4 == 0){
+					array[row][col] = "|";
+				}
+			}
+		}
+		
+		printPic(array);
+	}
+	
+	private static void printPic(String[][] arr) {
+		for(String[] row : arr){
+			for(String col : row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}
 	}
 	
 	/*
