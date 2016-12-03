7287f284c1d529a7388091a33fd8c990a4d45e9d
Riaked <Riaked@DESKTOP-UVEVF3A.home>
2016-12-03 07:56:27 -0500

    3 Files

---
+++ Cavemanmanmanman/src/haoSyed/HaoRandomize.java
@@ -0,0 +1,63 @@
+package haoSyed;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import caveExplorer.CaveExplorer;
+
+public class HaoRandomize {
+	
+	static Scanner input = new Scanner(System.in);
+	
+	
+	public static String waitForEntry(){
+		return input.nextLine();
+	}
+
+	public static int[][] randomizer1() {
+		int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
+	    int board1[][] = new int[4][4];
+	    Random random = new Random();
+	    int temp, t;
+	    for (int j = 0; j <= 20; j++){
+	        for (int x = 0; x < 16; x++){
+	             t = random.nextInt(1000) % 15;
+	             temp = num[x];
+	             num[x] = num[t];
+	             num[t] = temp;
+	        }
+	        t = 0;
+	        for (int r = 0; r < 4; r++){
+	            for (int s = 0; s < 4; s++){
+	                board1[r][s] = num[t];
+	                t = t + 1;
+	            }
+	        }
+	    }
+	       return board1;
+	 }
+	
+	 public static int[][] randomizer2() {
+		 int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
+	     int board2[][] = new int[4][4];
+	     Random random = new Random();
+	     int temp, t;
+	     for (int j = 0; j <= 20; j++){
+	         for (int x = 0; x < 16; x++){
+	             t = random.nextInt(1000) % 15;
+	             temp = nums[x];
+	             nums[x] = nums[t];
+	             nums[t] = temp;
+	         }
+	         t = 0;
+	         for (int r = 0; r < 4; r++){
+	             for (int s = 0; s < 4; s++){
+	                 board2[r][s] = nums[t];
+	                 t = t + 1;
+	             }
+	         } 
+	     }
+	     return board2;
+	     
+	 }
+}
--- Cavemanmanmanman/src/haoSyed/TilesGameStart.java
+++ Cavemanmanmanman/src/haoSyed/TilesGameStart.java
@@ -23,18 +23,15 @@ public class TilesGameStart implements Playable {
 	public static String waitForEntry(){
 		return input.nextLine();
 	}
-	
-	
-	//ASK NOCKLES MONDAY
+
 		public void play() {
 			readSequence(SEQUENCE_1);
 			int board1FlippedCards = 16;
 			int board2FlippedCards = 16;
-			displayBoard1(flippedCardsBoard1, board1);
-			displayBoard2(flippedCardsBoard2, board2);
-			int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-			board1 = randomizer1();
-			board2 = randomizer2();
+			SyedDisplayBoard.displayBoard1(flippedCardsBoard1, board1);
+			SyedDisplayBoard.displayBoard2(flippedCardsBoard2, board2);
+			board1 = HaoRandomize.randomizer1();
+			board2 = HaoRandomize.randomizer2();
 			while (board1FlippedCards > 0 && board2FlippedCards > 0){
 				System.out.println("Which board would you like to play from?");
 				String selection = input.nextLine();
@@ -58,13 +55,13 @@ public class TilesGameStart implements Playable {
 							flippedCardsBoard2[i][j] = false;
 						}
 					}
-					board1 = randomizer1();
-					board2 = randomizer2();
-					displayBoard1(flippedCardsBoard1, board1);
-					displayBoard2(flippedCardsBoard2, board2);
+					board1 = HaoRandomize.randomizer1();
+					board2 = HaoRandomize.randomizer2();
+					SyedDisplayBoard.displayBoard1(flippedCardsBoard1, board1);
+					SyedDisplayBoard.displayBoard2(flippedCardsBoard2, board2);
 				}
 				else if (selection.equals("1")){
-					displayBoard1(flippedCardsBoard1, board1);
+					SyedDisplayBoard.displayBoard1(flippedCardsBoard1, board1);
 					System.out.println("Enter the coordinates for your first card");
 					String x = input.nextLine();
 					if (x.length() != 2){
@@ -95,7 +92,7 @@ public class TilesGameStart implements Playable {
 								flippedCardsBoard2[x1][x2] = true;
 								flippedCardsBoard2[y1][y2] = true;
 								board2FlippedCards -= 2;
-								displayBoard1(flippedCardsBoard1, board1);
+								SyedDisplayBoard.displayBoard1(flippedCardsBoard1, board1);
 							}
 							else{
 								System.out.println("Try again.");
@@ -105,7 +102,7 @@ public class TilesGameStart implements Playable {
 				}
 				else if (selection.equals("2")){
 					System.out.println("Enter the coordinates for your first card");
-					displayBoard2(flippedCardsBoard2, board2);
+					SyedDisplayBoard.displayBoard2(flippedCardsBoard2, board2);
 					String i = input.nextLine();
 					if (i.length() != 2){
 						System.out.println("You need to put 2 numbers.");
@@ -135,6 +132,7 @@ public class TilesGameStart implements Playable {
 								flippedCardsBoard2[i1][i2] = true;
 								flippedCardsBoard2[j1][j2] = true;
 								board2FlippedCards -= 2;
+								SyedDisplayBoard.displayBoard2(flippedCardsBoard2, board2);
 							}
 							else{
 								System.out.println("Try again.");
@@ -146,106 +144,14 @@ public class TilesGameStart implements Playable {
 					System.out.println("That is not a valid board");
 				}
 			}
-			displayBoard1(flippedCardsBoard1, board1);
-			displayBoard2(flippedCardsBoard2, board2);
+			SyedDisplayBoard.displayBoard1(flippedCardsBoard1, board1);
+			SyedDisplayBoard.displayBoard2(flippedCardsBoard2, board2);
 			readSequence(SEQUENCE_2);
 		}
 	
-	public static void createGrid() {//adjust for haoSyed
-            for (int i = 0; i < 4; i++) {
-            	for (int a = 0; a < 4; a++) {
-                flippedCardsBoard1[i][a]=false;
-                flippedCardsBoard2[i][a]=false;
-                }
-            }
-   	}
 	public static void TilesGameStart() {
 		
 	}
-
-	private static void displayBoard2(boolean flippedCardsBoard2[][], int[][] board2) {//adjust boolean and int to haoSyed
-
-        System.out.println("   |     1     |     2     |     3     |     4     ");
-        for (int i = 0; i < 4; i++) {
-            System.out.print(" " + (i + 1) + " ");
-            for (int a = 0; a < 4; a++) {
-                if (flippedCardsBoard2[i][a]) {
-                    System.out.print("|     " + board2[i][a]+ "    ");
-                    System.out.print(" ");
-                }
-                else
-                    System.out.print("|     *     ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-	private static void displayBoard1(boolean flippedCardsBoard1[][], int[][] board1) {//adjust boolean and int to haoSyed
-
-        System.out.println("   |     1     |     2     |     3     |     4     ");
-        for (int i = 0; i < 4; i++) {
-            System.out.print(" " + (i + 1) + " ");
-            for (int a = 0; a < 4; a++) {
-                if (flippedCardsBoard1[i][a]) {
-                    System.out.print("|     " + board1[i][a]+ "    ");
-                    System.out.print(" ");
-                }
-                else
-                    System.out.print("|     ?     ");
-            }
-            System.out.println("");
-        }
-        System.out.println("");
-    }
-	
-	 public static int[][] randomizer1() {
-	       int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-	       int board1[][] = new int[4][4];
-	       Random random = new Random();
-	       int temp, t;
-	       for (int j = 0; j <= 20; j++){
-	           for (int x = 0; x < 16; x++){
-	               t = random.nextInt(1000) % 15;
-	               temp = num[x];
-	               num[x] = num[t];
-	               num[t] = temp;
-
-	           }
-	           t = 0;
-	           for (int r = 0; r < 4; r++){
-	               for (int s = 0; s < 4; s++){
-	                   board1[r][s] = num[t];
-	                    t = t + 1;
-	               }
-	           }
-	       }
-	       return board1;
-	   }
-	 public static int[][] randomizer2() {
-	        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-	        int board2[][] = new int[4][4];
-	        Random random = new Random();
-	        int temp, t;
-	        for (int j = 0; j <= 20; j++){
-	            for (int x = 0; x < 16; x++){
-	                t = random.nextInt(1000) % 15;
-	                temp = nums[x];
-	                nums[x] = nums[t];
-	                nums[t] = temp;
-
-	            }
-	            t = 0;
-	            for (int r = 0; r < 4; r++){
-	                for (int s = 0; s < 4; s++){
-	                    board2[r][s] = nums[t];
-	                    t = t + 1;
-	                }
-	            }
-
-	        }
-	        return board2;
-	    }
 	
 	public static void readSequence(String[] seq){
 		for(String s : seq){
